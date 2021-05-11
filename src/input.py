9fac0a391fb77e3a644eb8a7825853b45ace6e16
benhavenaar <b.havenaar@gmail.com>
2021-05-11 14:36:22 +0200

    able to find IP and append to list

--- src/input.py
+++ src/input.py
@@ -2,34 +2,38 @@
 import win32com.client
 from email.parser import HeaderParser
 import email
+import re
 
 class Input:
     def __init__(self, **kwargs):
-        self.outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-        self.message = self.outlook.OpenSharedItem(r"C:\Users\Ben\OneDrive - Ordina\emailanalyser\testmails\test.msg")
-        self.file = open(r"C:\Users\Ben\OneDrive - Ordina\emailanalyser\testmails\test6.eml", encoding="ISO-8859-1")
+        self.file = open(r"..\testmails\test5.eml", encoding="ISO-8859-1")
         self.headermessage = email.message_from_file(self.file)
         self.file.close()
         self.parser = email.parser.HeaderParser()
         self.header = self.parser.parsestr(self.headermessage.as_string())
-        
-    def printMessage(self):
-        print (self.message.SenderName)
-        print (self.message.SenderName)
-        print (self.message.SenderEmailAddress)
-        print (self.message.SentOn)
-        print (self.message.To)
-        print (self.message.CC)
-        print (self.message.BCC)
-        print (self.message.Subject)
-        print (self.message.Body)
+        self.emailArray = []
+        self.ipAddressRegex = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
         
     def printHeader(self):
         for h in self.header.items():
             print (h)
+            
+    def findIPAddress(self):
+        # self.emailArray.append(item for item in self.header.items() if self.ipAddressRegex in item)
+        for line in self.header.items():
+            # if line[1].search(self.ipAddressRegex):
+                # print (line)
+            if self.ipAddressRegex.search(line[1]) == None:
+                pass
+            else:
+                self.emailArray.append(self.ipAddressRegex.search(line[1]))
         
 input = Input()
-input.printMessage()
-input.printHeader()
+# print (input.header)
+input.findIPAddress()
+print(type(input.header))
+# print(input.header.items())
+input.findIPAddress()
+print(input.emailArray)
 
 
