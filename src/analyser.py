3747521506f43e12db198600f66bf5d4cf7b1d06
benhavenaar <b.havenaar@gmail.com>
2021-05-11 16:24:33 +0200

    Update analyser.py

--- src/analyser.py
+++ src/analyser.py
@@ -2,10 +2,17 @@
 
 class Analyser:
     def __init__(self):
-        
+        pass
 #variables
 
 #functions
+    def analyseIP(ipAddressList):
+        pass
+        
+    def analyseURL(urlList):
+        pass
     
+    def analyseAttachments(attachmentList):
+        pass
 #loop
 
