3c1a2da9a8e898d9cb84239c39a790f6db9cbd9d
benhavenaar <b.havenaar@gmail.com>
2021-05-27 15:47:29 +0200

    api url scan

--- src/analyser.py
+++ src/analyser.py
@@ -1,6 +1,7 @@
 #import libs
 import requests
 import json
+import base64
 
 class Analyser:
     def __init__(self):
@@ -8,6 +9,8 @@ class Analyser:
         self.urlResults = []
         self.attachmentResults = []
         self.url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
+        self.urlAnalysis = 'https://www.virustotal.com/api/v3/urls/'
+        self.headers = {"x-apikey": '4e5a399bbe79351e8f6533bde32337824bdba3e263e204d8a52849efbdd75e56'}
 #variables
 
 #functions
@@ -17,13 +20,19 @@ class Analyser:
         return response.json()['country']
         
     def analyseURL(self, urlList):
-        return self.urlResults
+        url_id = base64.urlsafe_b64encode(urlList[0].encode()).decode().strip("=")
+        print(self.urlAnalysis+url_id)
+        response = requests.get(self.urlAnalysis+url_id, headers=self.headers)
+        print(response)
+        result = json.loads(response.text)
+        print(result)
+        return result
     
     def analyseAttachments(self, attachmentList):
         return self.attachmentResults
         
     def jsonPrint(self, obj):
         text = json.dumps(obj, sort_keys=True, indent=4)
-        print(text)
+        # print(text)
 #loop
 
