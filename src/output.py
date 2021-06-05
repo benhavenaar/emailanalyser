4dc51eeef7866f97a94d6dd4fa2da95014438cc3
benhavenaar <b.havenaar@gmail.com>
2021-06-05 11:52:45 +0200

    Update output.py

--- src/output.py
+++ src/output.py
@@ -1,4 +1,10 @@
 #import libs
+#Output should contain the following information:
+# - Scanned email name/path
+# - SPF/DKIM/DMARC signature results
+# - URL + results
+# - Attachments + results
+
 
 class Output:
 pass
